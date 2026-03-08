5e76c4eebf2d45a4bd3faaf71cfd34c0e338d4c6
kashahmadi95-cmd <kash.ahmadi95@gmail.com>
2026-03-08 00:02:55 -0330

    Update data_loader.py

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,1 +1,22 @@
 
+import kagglehub
+import shutil
+import os
+
+# download dataset
+dataset_path = kagglehub.dataset_download(
+    "caesarmario/oecd-data-crude-oil-production"
+)
+
+print("Downloaded to:", dataset_path)
+
+# destination folder
+dest = "../data/raw"
+
+os.makedirs(dest, exist_ok=True)
+
+# copy files
+for file in os.listdir(dataset_path):
+    shutil.copy(os.path.join(dataset_path, file), dest)
+
+print("Dataset copied to:", dest)
