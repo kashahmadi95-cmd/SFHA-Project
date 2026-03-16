ddfce7d29e7ddcaaa3a98dde7bdc09e9e60f6874
kashahmadi95-cmd <kash.ahmadi95@gmail.com>
2026-03-16 20:37:39 -0230

    Update

--- src/data_loader.py
+++ src/data_loader.py
@@ -4,9 +4,7 @@ import shutil
 import os
 
 # download dataset
-dataset_path = kagglehub.dataset_download(
-    "sazidthe1/oil-production"
-)
+dataset_path = kagglehub.dataset_download("caesarmario/oecd-data-crude-oil-production")
 
 print("Downloaded to:", dataset_path)
 
@@ -20,3 +18,4 @@ for file in os.listdir(dataset_path):
     shutil.copy(os.path.join(dataset_path, file), dest)
 
 print("Dataset copied to:", dest)
+
